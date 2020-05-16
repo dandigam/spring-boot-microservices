9e8fe31cf11b003ba500288a8e7aaa074e0c072c
COOL ST@R <dandigam@gmail.com>
2020-05-16 12:39:50 +0530

    added user service

---
+++ user-service/src/test/java/io/micro/service/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.micro.service.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
